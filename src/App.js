62331ea3f89a0ff4f31e076df6fd51b1865d2179
farid.ansari <farid.ansari@gmail.com>
2024-06-15 20:17:45 -0700

    Impemented video change (states) and also smooth scrolling to top when new video is selected.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Navigation from "./components/Navigation";
 import VideoExplorer from "./components/VideoExplorer";
 import VideoList from "./components/VideoList";
 import videoList from "./data/videos.json"
+import {useState}  from "react";
 
 
 function App() {
+let [selectedVideo, setSelectedVideo] = useState(0)
 
 const videoClickHandler = (videoId) =>{
   console.log(videoId);
+  let x = videoList.findIndex((obj) => {return obj.id === videoId})
+  setSelectedVideo(x)
+  window.scrollTo({top:0, left:0, behavior:"smooth"});
 }
+
 
   return (
     <div className="App">
@@ -18,10 +24,10 @@ const videoClickHandler = (videoId) =>{
         <Navigation/>
       </div>
       <div>
-        <VideoExplorer index={0}/>
+        <VideoExplorer index={selectedVideo}/>
       </div>
       <div>
-        <VideoList list={videoList} clickHandler={videoClickHandler}/>
+        <VideoList list={videoList} clickHandler={videoClickHandler} selected={selectedVideo}/>
       </div>
     </div>
   );
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,16 +1,18 @@
 import "./VideoList.scss";
 import VideoListCard from "./VideoListCard";
 
-export default function VideoList({ list, clickHandler }) {
+export default function VideoList({ list, clickHandler, selected }) {
   return (
     <div>
       <div className="VideoList">
         <p className="font-Body font-Faded">NEXT VIDEOS</p>
       </div>
       <div>
-        {list.map((i) => (
-          <VideoListCard videoItem={i} clickHandler={clickHandler} key={i.id}/>
-        ))}
+        {list
+          .filter((e, i) => i !== selected)
+          .map((i) => (
+            <VideoListCard videoItem={i} clickHandler={clickHandler} key={i.id} />
+          ))}
       </div>
     </div>
   );
