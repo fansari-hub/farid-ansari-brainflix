a29f1a3bce2a0e7d2a80ff1fe8061a54bca7ed16
farid.ansari <farid.ansari@gmail.com>
2024-06-15 20:34:04 -0700

    Desktop Layout Experimentation.

--- src/App.js
+++ src/App.js
@@ -3,31 +3,33 @@ import "./App.scss";
 import Navigation from "./components/Navigation";
 import VideoExplorer from "./components/VideoExplorer";
 import VideoList from "./components/VideoList";
-import videoList from "./data/videos.json"
-import {useState}  from "react";
-
+import videoList from "./data/videos.json";
+import { useState } from "react";
 
 function App() {
-let [selectedVideo, setSelectedVideo] = useState(0)
+  let [selectedVideo, setSelectedVideo] = useState(0);
 
-const videoClickHandler = (videoId) =>{
-  console.log(videoId);
-  let x = videoList.findIndex((obj) => {return obj.id === videoId})
-  setSelectedVideo(x)
-  window.scrollTo({top:0, left:0, behavior:"smooth"});
-}
-
+  const videoClickHandler = (videoId) => {
+    console.log(videoId);
+    let x = videoList.findIndex((obj) => {
+      return obj.id === videoId;
+    });
+    setSelectedVideo(x);
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
 
   return (
     <div className="App">
-      <div>
-        <Navigation/>
+      <div className="App__navigation">
+        <Navigation />
       </div>
-      <div>
-        <VideoExplorer index={selectedVideo}/>
-      </div>
-      <div>
-        <VideoList list={videoList} clickHandler={videoClickHandler} selected={selectedVideo}/>
+      <div className="App__mainContent">
+        <div className="App__mainContent__videoExplorer">
+          <VideoExplorer index={selectedVideo} />
+        </div>
+        <div classname="App__mainContent__videoList">
+          <VideoList list={videoList} clickHandler={videoClickHandler} selected={selectedVideo} />
+        </div>
       </div>
     </div>
   );
