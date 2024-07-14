7de48ddb08c79b078a1d6d92b28ba10e9a6362d1
farid.ansari <farid.ansari@gmail.com>
2024-07-14 12:51:52 -0700

    Converted from JSON Data source to API Data source for existing pages.

--- src/components/VideoExplorer/VideoExplorer.js
+++ src/components/VideoExplorer/VideoExplorer.js
@@ -1,3 +1,4 @@
+import "./VideoExplorer.scss";
 import VideoPlayer from "./VideoPlayer/VideoPlayer";
 import GenericTitle from "../GenericTitle/GenericTitle";
 import VideoInfo from "./VideoInfo/VideoInfo";
@@ -5,52 +6,73 @@ import TextBlock from "../TextBlock/TextBlock";
 import CommentInput from "../CommentsInput/CommentInput";
 import Counter from "../Counter/Counter";
 import CommentsList from "../CommentsList/CommentsList";
-import videoData from "../../data/video-details.json";
 import VideoList from "./VideoList/VideoList";
-import videoListData from "../../data/videos.json";
-import { useState } from "react";
-import "./VideoExplorer.scss";
+
+import { useState, useEffect } from "react";
+import webapi from "../../webapi.js";
+import axios from "axios";
 
 export default function VideoExplorer() {
-  let [selectedVideo, setSelectedVideo] = useState(0);
+  let [selectedVideo, setSelectedVideo] = useState(null);
+  let [videoListData, setVideoListData] = useState([]);
+  let [videoData, setVideoData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(webapi.URL + "/videos" + webapi.KEY);
+      setVideoListData(response.data);
+      setSelectedVideo(response.data[0].id);
+      const response_video = await axios.get(webapi.URL + "/videos/" + response.data[0].id + webapi.KEY);
+      setVideoData(response_video.data);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (selectedVideo) {
+        const response = await axios.get(webapi.URL + "/videos/" + selectedVideo + webapi.KEY);
+        setVideoData(response.data);
+      }
+    };
+    fetchData();
+  }, [selectedVideo]);
 
   const videoClickHandler = (videoId) => {
-    //console.log(videoId);
-    let x = videoListData.findIndex((obj) => {
-      return obj.id === videoId;
-    });
-    setSelectedVideo(x);
+    setSelectedVideo(videoId);
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   };
 
-  return (
-    <div className="VideoExplorer">
-      <div className="VideoExplorer__videoPlayer">
-        <VideoPlayer image={videoData[selectedVideo].image} />
-      </div>
-      <div className="VideoExplorer__cont">
-        <div className="VideoExplorer__cont__meta">
-          <div className="VideoExplorer__cont__meta__videoTitle">
-            <GenericTitle title={videoData[selectedVideo].title} />
+  if (videoData) {
+    return (
+      <div className="VideoExplorer">
+        <div className="VideoExplorer__videoPlayer">
+          <VideoPlayer image={videoData.image} />
+        </div>
+        <div className="VideoExplorer__cont">
+          <div className="VideoExplorer__cont__meta">
+            <div className="VideoExplorer__cont__meta__videoTitle">
+              <GenericTitle title={videoData.title} />
+            </div>
+            <section className="VideoExplorer__cont__meta__videoInfo">
+              <VideoInfo author={videoData.channel} views={videoData.views} videoDate={videoData.timestamp} likes={videoData.likes} />
+            </section>
+            <article className="VideoExplorer__cont__meta__textBlock">
+              <TextBlock text={videoData.description} />
+            </article>
+            <section className="VideoExplorer__cont__meta__commentInput">
+              <Counter title="Comments" counter={videoData.comments.length} />
+              <CommentInput />
+            </section>
+            <section className="VideoExplorer__cont__meta__commentList">
+              <CommentsList comments={videoData.comments} />
+            </section>
           </div>
-          <section className="VideoExplorer__cont__meta__videoInfo">
-            <VideoInfo author={videoData[selectedVideo].channel} views={videoData[selectedVideo].views} videoDate={videoData[selectedVideo].timestamp} likes={videoData[selectedVideo].likes} />
-          </section>
-          <article className="VideoExplorer__cont__meta__textBlock">
-            <TextBlock text={videoData[selectedVideo].description} />
-          </article>
-          <section className="VideoExplorer__cont__meta__commentInput">
-            <Counter title="Comments" counter={videoData[selectedVideo].comments.length} />
-            <CommentInput />
-          </section>
-          <section className="VideoExplorer__cont__meta__commentList">
-            <CommentsList comments={videoData[selectedVideo].comments} />
+          <section className="VideoExplorer__cont__videoList">
+            <VideoList list={videoListData} clickHandler={videoClickHandler} selected={selectedVideo} />
           </section>
         </div>
-        <section className="VideoExplorer__cont__videoList">
-          <VideoList list={videoListData} clickHandler={videoClickHandler} selected={selectedVideo} />
-        </section>
       </div>
-    </div>
-  );
+    );
+  } else return <p>Loading Data ... Please Wait.</p>;
 }
---
+++ src/webapi.js
@@ -0,0 +1,6 @@
+const webapi = {
+    "KEY" : "?api_key=74dac765-1d47-4ae9-9f0f-efe375c27455",
+    "URL" : "https://unit-3-project-api-0a5620414506.herokuapp.com",
+}
+
+export default webapi;
