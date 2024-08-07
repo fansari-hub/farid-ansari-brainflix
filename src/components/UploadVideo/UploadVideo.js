33f6337e42e5717c2736653a3e9531575bd2b844
farid.ansari <farid.ansari@gmail.com>
2024-08-07 00:33:38 -0700

    implemented custom image functionality for new videos.

--- src/components/UploadVideo/UploadVideo.js
+++ src/components/UploadVideo/UploadVideo.js
@@ -6,10 +6,14 @@ import "./UploadVideo.scss";
 import webapi from "../../utils/webapi";
 import axios from "axios";
 
+
 const initialValues = {
   videoTitle: "",
   videoDescription: "",
+  videoImageFile: "",
+  videoImageURL: videoThumb
 };
+
 
 export default function UploadVideo() {
   const navigate = useNavigate();
@@ -23,7 +27,12 @@ export default function UploadVideo() {
     event.preventDefault();
       try {
         const postURL = webapi.URL + `/videos` + webapi.KEY;
-        await axios.post(postURL, { "title": values.videoTitle, "description": values.videoDescription, "imageName": "Upload-video-preview.jpg", "userName": userName});
+        const config = {headers: {'Content-Type': 'multipart/form-data'}}
+        if (values.videoImageFile === ""){
+          await axios.post(postURL, { "title": values.videoTitle, "description": values.videoDescription, "userName": userName});
+        } else{
+          await axios.post(postURL, { "title": values.videoTitle, "description": values.videoDescription, "userName": userName, "imageFile" : values.videoImageFile}, config);
+        }
         alert("Your video titled " + values.videoTitle + " is now uploading in the background.");
         navigate("/");
       } catch (error) {
@@ -50,6 +59,14 @@ export default function UploadVideo() {
     });
   };
 
+  const handleImgChange = (event) => {
+    setValues({
+      ...values,
+      "videoImageFile": event.target.files[0],
+      "videoImageURL": URL.createObjectURL(event.target.files[0]),
+    });
+  }
+
   const isFormValid = () => {
     if (values.videoDescription === "" || values.videoTitle === "") {
       return false;
@@ -62,10 +79,13 @@ export default function UploadVideo() {
       Upload Video
     </h1>
       <hr className="UploadVideo__ruler" />
-      <form className="UploadVideo" onSubmit={handleFormSubmit} ref={formRef}>
+      <form className="UploadVideo" onSubmit={handleFormSubmit} encType={'multipart/form-data'} ref={formRef}>
         <div className="UploadVideo__container">
           <label className="UploadVideo__container__title font-Faded font-Label">VIDEO THUMBNAIL</label>
-          <img className="UploadVideo__container__preview" src={videoThumb} alt="video preview" />
+          <img className="UploadVideo__container__preview" src={values.videoImageURL} alt="video preview" />
+          <div className = "UploadVideo__container__imgButton">
+            <input type="file" name="videoImg" accept="image/*" onChange={handleImgChange}/>
+            </div> 
         </div>
         <div className="UploadVideo__fieldgroup">
           <div className="UploadVideo__container">
